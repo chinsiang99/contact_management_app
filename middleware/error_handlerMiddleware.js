57079f335619dbb8bae940a24d3ece95d758fe80
chinsiang99 <chinsiang99@gmail.com>
2022-10-30 18:54:08 +0800

    update error handler message

--- middleware/error_handlerMiddleware.js
+++ middleware/error_handlerMiddleware.js
@@ -1,6 +1,4 @@
 const expressErrorHandler = (err, req, res, next) => {
-
-    // check whether has status code, if not then return 500 status code
     const statusCode = res.statusCode ? res.statusCode : 500;
     res.status(statusCode).json({
         status: statusCode,
