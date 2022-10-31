2dcf166d5e7ccac4614b68af91ba9a95955ac659
chinsiang99 <chinsiang99@gmail.com>
2022-11-01 00:20:27 +0800

    adding validateToken middleware to contact route

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
+const validateToken = require("../middleware/validateTokenHandler");
 
 const router = Router();
 
 // define routes after the "/contacts/" prefix
 // api/contacts/
+
+router.use(validateToken);
 
 router.route("/").get((req, res, next) => {
     res.json({
