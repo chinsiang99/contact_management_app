feb6662271c7d19c0ed077f2966775ec4e9f806b
chinsiang99 <chinsiang.tan@xeersoft.com>
2022-11-01 14:26:25 +0800

    add validate user existence middleware

---
+++ middleware/validateUserExist.js
@@ -0,0 +1,27 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const validateToken = asyncHandler(async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader && authHeader.startsWith("Bearer")) {
+        token = authHeader.split(" ")[1];
+        // console.log(process.env.ACCESS_TOKEN_SECERT);
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECERT, (err, decoded) => {
+            if (err) {
+                res.status(401);
+                throw new Error("User is not authorized");
+            }
+            req.user = decoded.user;
+            next();
+        });
+    } else {
+        throw new Error("User is not authorized");
+    }
+
+});
+
+module.exports = validateToken
+
+
