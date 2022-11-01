61a0e34d00abf338fa1534dd0ab9542eb5ed9f5f
chinsiang99 <chinsiang.tan@xeersoft.com>
2022-11-01 15:05:59 +0800

    update contact controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -20,7 +20,26 @@ const getAllContacts = asyncHandler(async (req,res,next)=>{
         res.status(500);
         throw new Error(errorMessage);
     }
-})
+});
+
+// get specific contact details with contact id
+const getSpecificContact = asyncHandler(async (req,res,next)=>{
+    const {contact_id} = req.params;
+
+    try{
+        const getSpecificContact = await Contact.where("_id").equals(contact_id);
+
+        res.status(200).json({
+            status: 200,
+            message: "Get Specific Contact Details Successfully",
+            contactDetails: getSpecificContact[0]
+        });
+    }catch(e){
+        const errorMessage = e.message;
+        res.status(500);
+        throw new Error(errorMessage);
+    }
+});
 
 const createContact = asyncHandler(async (req,res,next)=>{
     const {id} = req.user;
@@ -56,112 +75,9 @@ const createContact = asyncHandler(async (req,res,next)=>{
         res.status(500);
         throw new Error(errorMessage);
     }
-    
-    // const 
-    // res.json({
-    //     message: "hello there"
-    // })
-})
-
-const registerUser = asyncHandler(async (req, res, next) => {
-    const { username, email, password } = req.body;
-
-    // regex for email
-    const regex = /\S+@\S+\.\S+/;
-    // const regex2 = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-
-    // check either username, email or password is not filled
-    if (!username || !email || !password) {
-        res.status(400);
-        throw new Error("All fields are mandatory!");
-    }
-
-    // email format validation
-    if (!email.match(regex)) {
-        res.status(400);
-        throw new Error("Invalid email format, please insert a valid email format");
-    }
-
-    const duplicateEmail = await User.where("email").equals(email).limit(1);
-
-    if (duplicateEmail.length > 0) {
-        throw new Error(`Duplicate email: ${email}`);
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    try {
-        const createUser = await User.create({
-            username: username,
-            email: email,
-            password: hash
-        });
-        console.log(createUser);
-        res.status(201).json({
-            message: "Register Successfully!"
-        })
-    } catch (e) {
-        const errorMessage = e.message;
-        res.status(500);
-        throw new Error(errorMessage);
-    }
 
 });
 
-const loginUser = asyncHandler(async (req, res, next) => {
-    const { email, password } = req.body;
 
-    // check either email or password is not filled
-    if (!email || !password) {
-        res.status(400);
-        throw new Error("All fields are mandatory!");
-    }
 
-    // check whether the email is registered
-    const user = await User.where("email").equals(email);
-    if (!user.length) {
-        res.status(401);
-        throw new Error("Email is not registered");
-    }
-
-    // compare the password to check whether they match each other
-    authorized = await bcrypt.compare(password, user[0].password);
-
-    if (authorized) {
-        const accessToken = jwt.sign(
-            {
-                user: {
-                    id: user[0]._id,
-                },
-            },
-            process.env.ACCESS_TOKEN_SECERT,
-            { expiresIn: "24h" }
-        );
-        res.status(200).json({
-            token: accessToken
-        })
-
-    } else {
-        res.status(401);
-        throw new Error("Invalid password");
-    }
-})
-
-const getCurrentUser = asyncHandler(async (req, res, next) => {
-    const { id } = req.user;
-    const currentUser = await User.where("_id").equals(id);
-
-    if (currentUser.length > 0) {
-        res.status(200).json({
-            status: 200,
-            message: "Retrieve Current User Detail Successful",
-            userDetail: currentUser[0]
-        })
-    } else {
-        res.status(400);
-        throw new Error("No Record found");
-    }
-})
-
-module.exports = { createContact, getAllContacts, registerUser, loginUser, getCurrentUser }
+module.exports = { createContact, getAllContacts, getSpecificContact}
