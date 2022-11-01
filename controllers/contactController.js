6ffd322d5e1bbf8519f8c85f8df4822e15b9cc0b
chinsiang99 <chinsiang99@gmail.com>
2022-11-01 21:50:43 +0800

    update contact controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,17 +4,17 @@ const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
 // getting all contacts
-const getAllContacts = asyncHandler(async (req,res,next)=>{
-    const {id} = req.user;
+const getAllContacts = asyncHandler(async (req, res, next) => {
+    const { id } = req.user;
 
-    try{
+    try {
         const getAllContacts = await Contact.where("User").equals(id);
         res.status(200).json({
             status: 200,
             message: "Get All Contacts Successfully",
             contactList: getAllContacts
         });
-    }catch(e){
+    } catch (e) {
         const errorMessage = e.message;
         res.status(500);
         throw new Error(errorMessage);
@@ -22,10 +22,10 @@ const getAllContacts = asyncHandler(async (req,res,next)=>{
 });
 
 // get specific contact details with contact id
-const getSpecificContact = asyncHandler(async (req,res,next)=>{
-    const {contact_id} = req.params;
+const getSpecificContact = asyncHandler(async (req, res, next) => {
+    const { contact_id } = req.params;
 
-    try{
+    try {
         const getSpecificContact = await Contact.where("_id").equals(contact_id);
 
         res.status(200).json({
@@ -33,7 +33,7 @@ const getSpecificContact = asyncHandler(async (req,res,next)=>{
             message: "Get Specific Contact Details Successfully",
             contactDetails: getSpecificContact[0]
         });
-    }catch(e){
+    } catch (e) {
         const errorMessage = e.message;
         res.status(500);
         throw new Error(errorMessage);
@@ -41,12 +41,12 @@ const getSpecificContact = asyncHandler(async (req,res,next)=>{
 });
 
 // create new contact
-const createContact = asyncHandler(async (req,res,next)=>{
-    const {id} = req.user;
-    const {name, email, phone} = req.body;
+const createContact = asyncHandler(async (req, res, next) => {
+    const { id } = req.user;
+    const { name, email, phone } = req.body;
 
     // check whether every fields are filled in
-    if(!name || !email || !phone){
+    if (!name || !email || !phone) {
         res.status(400);
         throw new Error("All fields are mandatory!");
     }
@@ -78,12 +78,12 @@ const createContact = asyncHandler(async (req,res,next)=>{
 });
 
 // update contact with contact id
-const updateContact = asyncHandler(async(req,res,next)=>{
-    const {contact_id} = req.params;
-    const {name, email, phone} = req.body;
+const updateContact = asyncHandler(async (req, res, next) => {
+    const { contact_id } = req.params;
+    const { name, email, phone } = req.body;
 
     // check whether every fields are filled in
-    if(!name || !email || !phone){
+    if (!name || !email || !phone) {
         res.status(400);
         throw new Error("All fields are mandatory!");
     }
@@ -96,22 +96,23 @@ const updateContact = asyncHandler(async(req,res,next)=>{
         throw new Error("Invalid email format, please insert a valid email format");
     }
 
-    try{
+    try {
         const updateContact = await Contact.where("_id").equals(contact_id);
-        if(updateContact.length > 0){
+        if (updateContact.length > 0) {
             updateContact[0].name = name;
             updateContact[0].email = email;
             updateContact[0].phone = phone;
+            updateContact[0].updatedAt = Date.now();
             updateContact[0].save();
             res.status(200).json({
                 status: 200,
                 message: "Update Contact Successfully",
             });
-        }else{
+        } else {
             res.status(400);
             throw new Error("Specific Contact Does Not Exist!");
-        }   
-    }catch(e){
+        }
+    } catch (e) {
         const errorMessage = e.message;
         res.status(500);
         throw new Error(errorMessage);
@@ -120,21 +121,21 @@ const updateContact = asyncHandler(async(req,res,next)=>{
 });
 
 // delete specific contact with contact id
-const deleteContact = asyncHandler(async(req,res,next)=>{
-    const {contact_id} = req.params;
-    try{
+const deleteContact = asyncHandler(async (req, res, next) => {
+    const { contact_id } = req.params;
+    try {
         const deleteContact = await Contact.where("_id").equals(contact_id);
-        if(deleteContact.length > 0){
+        if (deleteContact.length > 0) {
             deleteContact[0].remove();
             res.status(200).json({
                 status: 200,
                 message: "Delete Contact Successfully"
             })
-        }else{
+        } else {
             res.status(400);
             throw new Error("Specific Contact Does Not Exist!");
         }
-    }catch(e){
+    } catch (e) {
         const errorMessage = e.message;
         res.status(500);
         throw new Error(errorMessage);
@@ -142,4 +143,4 @@ const deleteContact = asyncHandler(async(req,res,next)=>{
 })
 
 
-module.exports = { createContact, getAllContacts, getSpecificContact, updateContact, deleteContact}
+module.exports = { createContact, getAllContacts, getSpecificContact, updateContact, deleteContact }
