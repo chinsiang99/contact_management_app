4372e10697081b97c2a2eb4a0f2882a5248a2e4c
chinsiang99 <chinsiang.tan@xeersoft.com>
2022-11-02 15:57:35 +0800

    update readme file

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require("express");
-// const express = require("express-async-handler");
-
 const { expressErrorHandler } = require("./middleware/error_handlerMiddleware");
 require("./config/dbConnection");
 const { router } = require("./routes/index");
 
 const app = express();
 app.use(express.json());
-
-// app.get("/normal-way", (req, res, next) => {
-//     res.json({
-//         text: "Hello there"
-//     })
-// })
 
 // api routes
 app.use("/api/", router);
